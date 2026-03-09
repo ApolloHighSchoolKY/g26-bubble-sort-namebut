1ed7c3b6bf878b5ac8647a628ccf92a773ca48da
Brenden <92991638+namebut@users.noreply.github.com>
2026-03-09 19:25:29 +0000

    commit

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,29 +1,42 @@
+import java.util.Arrays;
+
 public class BubbleSort
 {
-
 	public static void main(String[] args)
   	{
-    	int[] myNums = {12, 15, 0, 44, 13, 1, 2};
+    	int[] unsorted = {12, 15, 0, 44, 13, 1, 2};
+		int[] sorted;
+		
+		System.out.println(Arrays.toString(unsorted));
+		sorted = SortTheBubble(unsorted);
+		System.out.println(Arrays.toString(sorted));
+  }
+
+  	public static int[] SortTheBubble(int[] unsorted)
+	{
+		int[] sorted = unsorted;
+		
 		int passes;
 		int comparisons;
 		int tempNum;
 		//Loop once for each pass, where passes is one less than the number of items.
-		passes = (myNums.length - 1);
+		passes = (sorted.length - 1);
 		for(int i = 1; i <= passes; i++)
 		{
 			//Loop once for each comparison, where comparisons are one less than the number of unsorted.
-			comparisons = (myNums.length - i);
+			comparisons = (sorted.length - i);
 			for(int x = 0; x < comparisons; x++)
 			{
 				//If they are out of order, swap the values
-				if(myNums[x] > myNums[x+1])
+				if(sorted[x] > sorted[x+1])
 				{
-					tempNum = myNums[x];
-					myNums[x] = myNums[x+1];
-					myNums[x+1] = tempNum;
+					tempNum = sorted[x];
+					sorted[x] = sorted[x+1];
+					sorted[x+1] = tempNum;
 				}
 			}
-			//The next pass will use one less comparison
+			
 		}
-  }
+		return sorted;
+	}
 }
